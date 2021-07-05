29fe729371cb0ad416e862df55ca49f0245fc903
Artur Sadov <Art.S.V.86@yandex.ru>
2021-07-05 22:01:50 +0400

    Added files

---
+++ src/test/java/ru/netology/app_card_delivery/AppCardDelivery.java
@@ -0,0 +1,49 @@
+package ru.netology.app_card_delivery;
+
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+class AppCardDelivery {
+
+    void dataInput(int days) {
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(DataUser.dataInput(days));
+    }
+    void dataInput2(int days) {
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(DataUser.dataInput2(days));
+    }
+
+    @BeforeEach
+    void before() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void getTrueInputValidForm() {
+        $("[placeholder=Город]").setValue(DataUser.cityForInput());
+        int inDays = 4;
+        dataInput(inDays);
+        $("[data-test-id=phone]").$("[name=phone]").setValue(DataUser.dataPhone());
+        $("[data-test-id=name].input_type_text .input__control").setValue(DataUser.dataName());
+        $("[data-test-id=agreement]").click();
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataUser.dataInput(inDays)));
+        int inDays2 = 3;
+        dataInput2(inDays2);
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $$("[class=button__text]").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataUser.dataInput2(inDays2)));
+    }
+
+}
